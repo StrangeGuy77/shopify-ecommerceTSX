951433f0f9ae40fbad102c467ee0667ffe2de84b
Jhonatan <jhonatanrg@live.com>
2020-02-07 11:04:13 -0500

    Testing first homepage

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../css/App.css";
+import HomePage from "./HomePage/HomePage";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="XD">
+        <HomePage />
+      </div>
+    );
+  }
+}

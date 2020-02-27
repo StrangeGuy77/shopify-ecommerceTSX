b3e2030582c6a0091a8d07412be0261b1e5c8bf0
Jhonatan <jhonatanrg@live.com>
2020-02-27 05:57:02 -0500

    Redux-persist integration completed.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,15 @@ import "./css/index.css";
 import App from "./components/App";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
     <Provider store={store} >
         <Router>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </Router>
     </Provider>
     , document.getElementById("root"));
--- frontend/src/redux/rootReducer.ts
+++ frontend/src/redux/rootReducer.ts
@@ -1,8 +1,18 @@
 import {userReducer} from "./user/userReducer";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./cart/cartReducer";
 import {combineReducers} from "redux";
 
-export default combineReducers({
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
